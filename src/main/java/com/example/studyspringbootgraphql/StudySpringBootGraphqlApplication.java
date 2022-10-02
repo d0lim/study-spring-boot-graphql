80ea7e890e4691af96837e3d7f98e70ecf18c839
d0lim <limdongyoung0@gmail.com>
2022-10-03 00:35:23 +0900

    Initialize repo

---
+++ src/main/java/com/example/studyspringbootgraphql/StudySpringBootGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyspringbootgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBootGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringBootGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studyspringbootgraphql/StudySpringBootGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyspringbootgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBootGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
